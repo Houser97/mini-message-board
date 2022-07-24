1355d858e029183aa78702adef226f9a024c6d8e
Houser97 <arturo.riverar97@gmail.com>
2022-07-24 16:13:13 -0500

    Create messages array at the top of index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+let messages = [
+  {
+    text: "I love chicken",
+    user: "Houser",
+    date: new Date(),
+  },
+  {
+    text: "I love pamtas and cheems",
+    user: "Cocco",
+    date: new Date(),
+  },
+  {
+    text: "I like lettuce, but love hay",
+    user: "Quemso",
+    date: new Date(),
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
